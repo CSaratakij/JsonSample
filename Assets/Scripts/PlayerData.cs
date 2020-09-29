327fd81b225eb47d43e452cbbb44a32cc67d0829
Chatchai Saratakij <CSaratakij@gmail.com>
2020-09-29 17:05:47 +0700

    Init Project

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class PlayerData
+{
+    public string UserID;
+    public string Name;
+    public Vector3 Position;
+
+    public override string ToString()
+    {
+        var result = $"UserID : {UserID}, ";
+        result += $"Position : x: {Position.x}, y: {Position.y}, z: {Position.z}";
+        return result;
+    }
+
+    public string ToStringDirectCall()
+    {
+        var result = $"UserID : {UserID}, ";
+        result += $"Position : {Position}";
+        return result;
+    }
+}
+
---
+++ Assets/Scripts/PlayerSync.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSync : MonoBehaviour
+{
+    string json;
+    PlayerData data;
+
+    void Awake()
+    {
+        Initialize();
+    }
+
+    void Start()
+    {
+        SyncFromJSon();
+    }
+
+    void Initialize()
+    {
+        var data = new PlayerData();
+
+        data.UserID = "weroiu32408324lkj";
+        data.Name = "Player 1";
+        data.Position = transform.position;
+
+        json = JsonUtility.ToJson(data);
+
+        Debug.Log("Initialize json string....");
+        Debug.Log(json);
+
+        Debug.Log("------------------------------");
+    }
+
+    void SyncFromJSon()
+    {
+        data = GetPlayerData(json);
+
+        Debug.Log("Print from PlayerData -> override ToString....");
+        Debug.Log(data);
+
+        Debug.Log("------------------------------");
+
+        Debug.Log("Print from Vector3 -> ToString call....");
+        Debug.Log(data.ToStringDirectCall());
+    }
+
+
+    PlayerData GetPlayerData(string json)
+    {
+        var data = JsonUtility.FromJson<PlayerData>(json);
+        return data;
+    }
+}
+
